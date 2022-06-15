b6b2f55b08fc1cee62ce1909011fbdc740f9ae93
chk-yourself <c.hyunakim@gmail.com>
2022-06-15 14:54:59 -0700

    update public path

--- src/components/Clouds.vue
+++ src/components/Clouds.vue
@@ -171,6 +171,22 @@ function init() {
   let lightHelper = new THREE.DirectionalLightHelper(light, 5);
   light.add(lightHelper);
 
+  // Background
+
+  // Stars
+  function addStar() {
+    const geometry = new THREE.SphereGeometry(0.25, 24, 24);
+    const material = new THREE.MeshStandardMaterial({ color: 0xffffff });
+    const star = new THREE.Mesh(geometry, material);
+    const [x, y, z] = Array(3)
+      .fill()
+      .map(() => THREE.MathUtils.randFloatSpread(100));
+    star.position.set(x, y, z);
+    scene.add(star);
+  }
+
+  Array(200).fill().forEach(addStar);
+
   /**
   GUI
 
